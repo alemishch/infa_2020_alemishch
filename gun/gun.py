3683a86ef6a43da753e11d619f1d6726c61132f6
alemishch <mishchenko.ad@phystech.edu>
2020-11-30 11:11:53 +0300

    Added 2 guns

--- gun/gun.py
+++ gun/gun.py
@@ -6,7 +6,7 @@ from PIL import ImageTk, Image
 
 W = 800
 H = 600
-dH = 0
+dH = 45
 dW = 60
 root = tk.Tk()
 fr = tk.Frame(root)
@@ -14,9 +14,15 @@ root.geometry('800x600')
 canv = tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
 canv.focus_set()
+num = 0
+def change_gun(event):
+    global num
+    num = (num + 1) % 2
+    print(num)
 
 canv.create_line(60, 0, 60, H)
 canv.create_line(W - 60, 0, W - 60, H)
+canv.create_line(0, H-45, W, H-45)
 
 
 def playGif(file, b):
@@ -43,19 +49,19 @@ class Bomb:
         self.vy = self.v * math.sin(angle)
         self.bomb = canv.create_oval(self.x - self.r, self.y + self.r, self.x + self.r, self.y - self.r, fill="yellow")
 
-    def move(self, obj, arr):
+    def move(self, guns, arr):
         self.x += self.vx
         self.y += self.vy
         canv.coords(self.bomb, self.x - self.r, self.y + self.r, self.x + self.r, self.y - self.r)
-        if (obj.y - 20 < self.y - self.r and obj.y+40 > self.y + self.r) and self.x - self.r < 60 and\
-                self.x + self.r > 0:
-            arr.remove(self)
-            canv.delete(self.bomb)
-            obj.hp -= 1
-            canv.itemconfig(self.bomb, fill="red")
         if self.x-self.r>W or self.x+self.r<0 or self.y-self.r>H or self.y+self.r<0:
             canv.delete(self.bomb)
             arr.remove(self)
+        for obj in guns:
+            if (obj.y - 20 < self.y - self.r and obj.y+40 > self.y + self.r) and self.x - self.r < 60 and\
+                    self.x + self.r > 0:
+                arr.remove(self)
+                canv.delete(self.bomb)
+                obj.hp -= 1
 
 
 class ball():
@@ -131,35 +137,55 @@ class ball():
 
 
 class gun():
-    def __init__(self):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         self.hp = 5
         self.img = Image.open("ufo.png")
         self.img = self.img.resize((60, 40), Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(self.img)
-        self.image = canv.create_image(0, 430, anchor=tk.NW, image=self.img)
-        self.ots = 40
-        self.y = 450
+        self.image = canv.create_image(self.x-20, self.y-20, anchor=tk.NW, image=self.img)
+        self.ots = self.x+20
         self.f2_power = 20
         self.f2_on = 0
         self.an = 1
-        self.id = canv.create_line(self.ots, self.y, self.ots + 20, 450, width=4)
+        self.id = canv.create_line(self.ots, self.y, self.ots + 20, self.y, width=4)
 
     def switch_weapon(self, event):
         self.weapon_type += 1
 
     def move(self):
-        canv.coords(self.id, self.ots, self.y, self.ots + self.f2_power * math.cos(self.an), self.y +
+        canv.coords(self.id, self.x+20, self.y, self.x+20 + self.f2_power * math.cos(self.an), self.y +
                     self.f2_power * math.sin(self.an))
 
     def up(self, event):
         if self.y - 20 > 0:
             self.y -= 5
             canv.move(self.image, 0, -5)
+            canv.coords(self.id, self.ots, self.y, self.ots + self.f2_power * math.cos(self.an), self.y +
+                        self.f2_power * math.sin(self.an))
+            print("")
 
     def down(self, event):
         if self.y < H - 20:
             self.y += 5
             canv.move(self.image, 0, 5)
+            canv.coords(self.id, self.ots, self.y, self.ots + self.f2_power * math.cos(self.an), self.y +
+                        self.f2_power * math.sin(self.an))
+
+    def left(self, event):
+        if self.x > 0:
+            self.x -= 5
+            canv.move(self.image, -5, 0)
+            canv.coords(self.id, self.x+20, self.y, self.x+20 + self.f2_power * math.cos(self.an), self.y +
+                        self.f2_power * math.sin(self.an))
+
+    def right(self, event):
+        if self.x < W:
+            self.x += 5
+            canv.move(self.image, 5, 0)
+            canv.coords(self.id, self.x+20, self.y, self.x+20 + self.f2_power * math.cos(self.an), self.y +
+                        self.f2_power * math.sin(self.an))
 
     def fire2_start(self, event):
         self.f2_on = 1
@@ -173,9 +199,9 @@ class gun():
         global balls
         new_ball = ball()
         new_ball.r += 5
-        self.an = math.atan((event.y - self.y) / (event.x - 20))
+        self.an = math.atan((event.y - self.y) / (event.x - self.x))
         new_ball.y = self.y + self.f2_power * math.sin(self.an)
-        new_ball.x = self.ots + self.f2_power * math.cos(self.an)
+        new_ball.x = self.x+20 + self.f2_power * math.cos(self.an)
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
         balls += [new_ball]
@@ -185,15 +211,18 @@ class gun():
     def targetting(self, event=0):
         """Прицеливание. Зависит от положения мыши."""
         if event:
-            self.an = math.atan((event.y - self.y) / (event.x - 20))
+            self.an = math.atan((event.y - self.y) / (event.x - self.x))
         if self.f2_on:
             canv.itemconfig(self.id, fill='orange')
         else:
             canv.itemconfig(self.id, fill='black')
-        canv.coords(self.id, self.ots, self.y,
-                    self.ots + self.f2_power * math.cos(self.an),
+        canv.coords(self.id, self.x+20, self.y,
+                    self.x+20 + self.f2_power * math.cos(self.an),
                     self.y + self.f2_power * math.sin(self.an)
                     )
+        if self.hp == 0:
+            canv.delete(self.id)
+            canv.itemconfig(screen1, text="You lost")
 
     def power_up(self):
         if self.f2_on:
@@ -216,7 +245,7 @@ class target():
     def move(self, z):
         self.x += self.vx
         self.y += self.vy
-        if self.y + self.r >= H - dH or self.y - self.r <= dH:
+        if self.y + self.r >= H - dH or self.y - self.r <= 0:
             self.vy *= -1
         if self.x - self.r <= dW or self.x + self.r >= W - dW:
             self.vx *= -1
@@ -252,7 +281,7 @@ class target():
         self.vx = rnd(-10, 10)
         self.vy = rnd(-10, 10)
         x = self.x = rnd(230, 700)
-        y = self.y = rnd(50, 550)
+        y = self.y = rnd(50, 500)
         r = self.r = rnd(2, 50)
         color = self.color = choice(['blue', 'green', 'red', 'brown'])
         canv.coords(self.id, x - r, y - r, x + r, y + r)
@@ -265,25 +294,27 @@ class target():
 
 
 screen1 = canv.create_text(400, 300, text='', font='28')
-g1 = gun()
+g1 = gun(20, 450)
+g2 = gun(300, 580)
 text = canv.create_text(100, 20, text="Your health: {}".format(g1.hp))
 
 balls = []
 
 
 def new_game(event=''):
-    global gun, screen1, balls, bombs
+    global gun, screen1, balls, bombs, num
+    guns = [g1, g2]
     enemies, victory = 1, 0
     g1.hp = 5
     balls = []
     bombs = []
     targets = []
     canv.itemconfig(screen1, text='')
-    canv.bind('<Button-1>', g1.fire2_start)
-    canv.bind('<ButtonRelease-1>', g1.fire2_end)
-    canv.bind('<Motion>', g1.targetting)
+    canv.bind('q', change_gun)
     canv.bind('w', g1.up)
     canv.bind('s', g1.down)
+    canv.bind('a', g2.left)
+    canv.bind('d', g2.right)
     for i in range(1, 5):
         targets.append(target())
     for t in targets:
@@ -292,16 +323,19 @@ def new_game(event=''):
     z = 0.03
     while enemies == 1:
         if len(balls):
-            g1.move()
+            guns[num].move()
             for t in targets:
+                canv.bind('<Button-1>', guns[num].fire2_start)
+                canv.bind('<ButtonRelease-1>', guns[num].fire2_end)
+                canv.bind('<Motion>', guns[num].targetting)
                 t.move(z)
                 t.set_coords()
                 t.explode(z, targets)
                 canv.itemconfig(text, text="")
                 for b in bombs:
-                    b.move(g1, bombs)
+                    b.move(guns, bombs)
             for b in balls:
-                canv.itemconfig(text, text="Your health: {}".format(g1.hp))
+                canv.itemconfig(text, text="Your health: {}".format(guns[num].hp))
                 enemies, victory = 0, 1
                 b.move(z)
                 b.set_coords()
@@ -319,20 +353,23 @@ def new_game(event=''):
                         enemies, victory = 1, 0
             canv.update()
             time.sleep(z)
-            g1.targetting()
-            g1.power_up()
+            guns[num].targetting()
+            guns[num].power_up()
         elif not victory:
             for t in targets:
+                canv.bind('<Button-1>', guns[num].fire2_start)
+                canv.bind('<ButtonRelease-1>', guns[num].fire2_end)
+                canv.bind('<Motion>', guns[num].targetting)
                 t.move(z)
                 t.set_coords()
-                canv.itemconfig(text, text="Your health: {}".format(g1.hp))
+                canv.itemconfig(text, text="Your health: {}".format(guns[num].hp))
                 for b in bombs:
-                    b.move(g1, bombs)
+                    b.move(guns, bombs)
                 t.explode(z, targets)
             canv.update()
             time.sleep(z)
-            g1.targetting()
-            g1.power_up()
+            guns[num].targetting()
+            guns[num].power_up()
     canv.itemconfig(screen1, text='Вы уничтожили все цели')
     for b in balls:
         b.destroy()
